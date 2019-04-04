ae907c2c60cac8dc4d98d6320e7449ae32bb8a3d
Xue <1099232082@qq.com>
2019-04-04 19:46:39 +0800

    commit

--- JavaScript实现动画效果/scripts/moveElement.js
+++ JavaScript实现动画效果/scripts/moveElement.js
@@ -1,25 +1,27 @@
 function moveElement(elementID, final_x, final_y, interval) {
+    if (!document.getElementById) return false;
+    if (!document.getElementById(elementID)) return false;
     var elem = document.getElementById(elementID);
     var xpos = parseInt(elem.style.left);
     var ypos = parseInt(elem.style.top);
-    if(xpos == final_x && ypos == final_y) {
+    if (xpos == final_x && ypos == final_y) {
         return true;
     }
-    if(xpos < final_x) {
+    if (xpos < final_x) {
         xpos++;
     }
-    if(xpos > final_x) {
+    if (xpos > final_x) {
         xpos--;
     }
-    if(ypos < final_y) {
+    if (ypos < final_y) {
         ypos++;
     }
-    if(ypos > final_y) {
+    if (ypos > final_y) {
         ypos--;
     }
     elem.style.left = xpos + "px";
     elem.style.top = ypos + "px";
-    var repeat = "moveElement('"+elementID+"', "+final_x+", "+final_y+", "+interval+")";
+    var repeat = "moveElement('" + elementID + "', " + final_x + ", " + final_y + ", " + interval + ")";
     movement = setTimeout(repeat, interval);
     // 注意：下面这种写法是错误的。
     // 上面的写法是要存储`repeat = `后面拼接出来的字符串，然后在setTimeout方法中传参。
