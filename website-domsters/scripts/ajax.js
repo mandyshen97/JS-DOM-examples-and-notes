bca52151967b1690c35ee30bf54f2bc9b6e8dbdf
Xue <1099232082@qq.com>
2019-04-04 21:24:37 +0800

    commit

---
+++ website-domsters/scripts/ajax.js
@@ -0,0 +1,18 @@
+function regen() {
+	// home
+	prepareSlideshow();
+	// about
+	prepareInternalnav();
+	// photos
+	preparePlaceholder();
+	prepareGallery();
+	// live
+	stripeTables();
+	highlightRows();
+	displayAbbreviations();
+	// contact
+	focusLabels();
+	prepareForms();
+}
+
+
